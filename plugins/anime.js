ec1933c1dbe8e1efb8a4e10b404290ee6b775041
AYANUKOJI-KEN <100865866+Yahya910@users.noreply.github.com>
2022-03-21 19:42:09 +0100

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -6,7 +6,7 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: true, desc: 'random anime image'}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: true, desc: 'صورة انمي عشوائية'}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -94,13 +94,13 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*Made By Queen Angella*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*يحيى*'})
 
     }));
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: false, desc:'random anime image '}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: false, desc:'صورة انمي عشوائية '}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -188,7 +188,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*Made By Queen Angella*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*عمك يحيى*'})
 
     }));
 }
